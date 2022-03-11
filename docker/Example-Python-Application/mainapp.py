92c6da6ddf2be03ef6e2742935d946ae12d4cec6
Amik <amik@legalfit.com>
2022-03-12 01:17:23 +0545

    Added Docker-compose for app

--- docker/Example-Python-Application/mainapp.py
+++ docker/Example-Python-Application/mainapp.py
@@ -1,12 +1,34 @@
+import redis
+
+import os
 from flask import Flask
-from flask import jsonify
+
 
 app = Flask(__name__)
+redis = redis.StrictRedis(host=os.environ.get('REDIS_HOST'),
+			  port=os.environ.get('REDIS_PORT'),
+			  db=os.environ.get('REDIS_DB'),
+			  password=os.environ.et('REDIS_PASSWORD')
 
-@app.route("/")
-def hello():
-   response = {"message":"First Application from Docker Image"}
-   return jsonify(response)
 
-if __name__ == "__main__":
-   app.run(host='0.0.0.0')
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/visitor')
+def visitor():
+    redis.incr('visitor')
+    visitor_num = redis.get('visitor').decode("utf-8")
+    return "Visitor: %s" % (visitor_num)
+
+
+@app.route('/visitor/reset')
+def reset_visitor():
+    redis.set('visitor', 0)
+    visitor_num = redis.get('visitor').decode("utf-8")
+    return "Visitor is reset to %s" % (visitor_num)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
